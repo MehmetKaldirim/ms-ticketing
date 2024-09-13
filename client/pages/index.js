9edb6791b521f366c6f801bddccb30e7a5e03519
Mehmet <mathdenizi@gmail.com>
2024-09-13 03:37:40 +0200

    using ssr inside cluster first way

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,17 +2,32 @@ import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  axios.get("/api/users/currentuser").catch((err) => {
-    console.log(err.message);
-  });
+  // axios.get("/api/users/currentuser").catch((err) => {
+  //   console.log(err.message);
+  // });
 
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async ({ req }) => {
+  let url;
 
-//   return response.data;
-// };
+  if (typeof window === "undefined") {
+    // We're on the server, so we need an absolute URL
+    const baseURL = req ? `http://${req.headers.host}` : ""; // Get host from request headers
+    url = `${baseURL}/api/users/currentuser`;
+  } else {
+    // We're on the client, so relative URL is fine
+    url = "/api/users/currentuser";
+  }
+  console.log(url);
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return { currentUser: null };
+  }
+};
 
 export default LandingPage;
