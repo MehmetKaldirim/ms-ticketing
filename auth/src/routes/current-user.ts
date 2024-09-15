e352bc69aaa568fc4deb5779fd936527864cd6dd
Mehmet <mathdenizi@gmail.com>
2024-09-15 04:51:21 +0200

    usage of common microservis as npm modul inside the other projects

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@math-web-5180/common";
 
 const router = express.Router();
 
