13b1f75122d5aa92da073fcc20eca6037849ac5e
Mehmet <mathdenizi@gmail.com>
2024-09-22 23:07:47 +0200

    fixing we are receiving from app currentuser and build.client information

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,11 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are sign in</h1> : <h1>You are not sign in</h1>;
 
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 //a way to use ssr inside the cluster
