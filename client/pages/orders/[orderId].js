dd0b11c81abc13cda57f5f1aff1c35f552a43535
Mehmet <mathdenizi@gmail.com>
2024-09-23 01:45:21 +0200

    using env variable from kubernete inside next react client side

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  // Access the environment variable
+  //   const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISH;
+  //   console.log("Stripe Publishable Key:", stripeKey);
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISH} // Use the environment variable here
+      />
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
