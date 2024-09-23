685742be8138a4cb462817e719033ca81e67bddb
Mehmet <mathdenizi@gmail.com>
2024-09-23 03:16:45 +0200

    usage of props and body merging inside useRequest custom hook

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import Router from "next/router";
+import useRequest from "../../hooks/useRequest";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push("/orders"),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -22,17 +32,16 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  // Access the environment variable
-  //   const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISH;
-  //   console.log("Stripe Publishable Key:", stripeKey);
-
   return (
     <div>
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
-        token={(token) => console.log(token)}
+        token={({ id }) => doRequest({ token: id })}
         stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISH} // Use the environment variable here
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
       />
+      {errors}
     </div>
   );
 };
